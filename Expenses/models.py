aa829d02abd53a5fd182509de56f46ceaf592559
Natnael-Negash <nnegash658@gmail.com>
2024-10-22 21:49:24 -0900

    models for expense

--- Expenses/models.py
+++ Expenses/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+
+class Expenses(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    amount = models.FloatField()
+    Description = models.TextField(blank=True, null=True)
+    date = models.DateField(default=now)
+
+    def __str__(self):
+        return f"{self.amount} - {self.category} - {self.date}"
+    
+    
